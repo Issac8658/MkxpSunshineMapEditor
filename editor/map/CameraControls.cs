d289fd812baefa7b9731606ab055554734df3cf1
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-06-16 22:12:02 +0300

    code blocks prepare(events editor)

--- editor/map/CameraControls.cs
+++ editor/map/CameraControls.cs
@@ -4,11 +4,18 @@ using System;
 
 public partial class CameraControls : Camera2D
 {
-	private double targetZoom = 1;
 	private Window window;
 
 	[Export]
 	public Control CameraControlZone;
+	[Export]
+	public bool BoundToMap = true;
+	[Export]
+	public double TargetZoom = 1;
+	[Export]
+	public double MinZoom = 1.0 / 16.0;
+	[Export]
+	public double MaxZoom = 128.0;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,15 +27,16 @@ public partial class CameraControls : Camera2D
 			{
 				if (eventButton.Pressed)
 					if (eventButton.ButtonIndex == MouseButton.WheelUp)
-						targetZoom *= 2;
+						TargetZoom *= 2;
 					else if (eventButton.ButtonIndex == MouseButton.WheelDown)
-						targetZoom *= 0.5;
-					targetZoom = Mathf.Clamp(targetZoom, 1.0 / 16.0, 128.0);
+						TargetZoom *= 0.5;
+					TargetZoom = Mathf.Clamp(TargetZoom, MinZoom, MaxZoom);
 			}
 			else if (@event is InputEventMouseMotion eventMotion)
 				if ((eventMotion.ButtonMask & MouseButtonMask.Middle) == MouseButtonMask.Middle)
 				{
 					GlobalPosition -= eventMotion.Relative / Zoom;
+					if (BoundToMap)
 					if (RPG_Data.CurrentMap != null)
 						GlobalPosition = new(
 							Mathf.Clamp(GlobalPosition.X, 0, RPG_Data.CurrentMap.Size.X * TilesetHelper.TILE_WIDTH),
@@ -43,7 +51,7 @@ public partial class CameraControls : Camera2D
 	public override void _Process(double delta)
 	{
 		Offset = -(CameraControlZone.GlobalPosition - new Vector2(window.Size.X - CameraControlZone.GlobalPosition.X - CameraControlZone.Size.X, 0)) / Zoom * 0.5f;
-		float res = (float)Mathf.Lerp(Zoom.X, targetZoom, 0.5);
+		float res = (float)Mathf.Lerp(Zoom.X, TargetZoom, 0.5);
 		Zoom = new(res, res);
 	}
 }
